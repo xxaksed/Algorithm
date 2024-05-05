1e8ff82d4c3eeb769d3dfd9674455d1410300a2f
xxaksed <aksed.336@mail.ru>
2024-05-05 12:41:16 +0300

    Mattrix

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-let array = [
-    [1, 2, 3, 4],
-    [12, 13, 14, 5],
-    [11, 16, 15, 6],
-    [10, 9, 8, 7],
-];
 
 function CreateNumArray(array) {
     let NumArray = [];
@@ -15,8 +9,12 @@ function CreateNumArray(array) {
     NumArray.sort((a, b) => a - b)
     return NumArray
 }
-
 console.log(CreateNumArray(array));
 
-// const expected = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+let array = [
+    [1, 2, 3, 4],
+    [12, 13, 14, 5],
+    [11, 16, 15, 6],
+    [10, 9, 8, 7],
+];
 
