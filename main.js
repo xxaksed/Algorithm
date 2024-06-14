9fa9abd97400182083eb64df39059dcc3ea3382c
xxaksed <aksed.336@mail.ru>
2024-06-14 20:16:00 +0300

    AlgoNextLVL

--- main.js
+++ main.js
@@ -1,50 +1,18 @@
-let array = [12, 3, 1, 2, -6, 5, -8, 6];
+let array = [5, 2, [7, -1], 3, [6, [-13, 8], 4]];
 
-function FindSum(array) {
-	let result = [];
-	for (let i = 0; i < array.length; i++) {
-		let start = 1;
-		let end = array.length - 1;
-		while (start < end) {
-			let sum = array[i] + array[start] + array[end];
-			if (sum == 0) {
-				result.push([array[i], array[start], array[end]]);
-			}
-			if (end != start + 1) {
-				end--;
-			} else {
-				end = array.length - 1;
-				start++;
-			}
-			if (start >= array.length - 1) {
-				i++;
-				start = i + 1;
-				end = array.length - 1;
-			}
-			console.log(i, start, end);
+function NumSum(array) {
+	let sum = 0;
+	let i = 0;
+	let depth = 1;
+	while (i < array.length) {
+		if (Array.isArray(array[i])) {
+			sum += depth * NumSum(array[i]);
+			depth++;
+		} else {
+			sum += array[i];
 		}
-		return result;
+		i++;
 	}
+	return sum;
 }
-
-console.log(FindSum(array));
-
-// 3, 6, 7;
-// 1, 5, 6;
-// 2, 4, 5;
-
-// function FindSum(array) {
-// 	let result = [];
-// 	for (let i = 0; i < array.length; i++) {
-// 		let start = 1;
-// 		let end = array.length - 1;
-// 	}
-// 	while (strat < end) {
-// 		if (sum == 0) {
-// 			result.push(array[i], array[y], array[z]);
-// 			return result;
-// 		}
-// 		else
-// 		start++;
-// 	}
-// }
+console.log(NumSum(array));
